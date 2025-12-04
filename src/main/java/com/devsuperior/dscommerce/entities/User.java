0d94908369f2a09fbf88cc20bf30c03d562fe38a
Luiz Sette <luiz.fernando.sette@gmail.com>
2025-12-04 14:19:39 -0300

    Configurando o H2

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,88 @@
+package com.devsuperior.dscommerce.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+   
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String name;
+   private String email;
+   private String fone;
+   private LocalDate birthDate;
+   private String password;
+   
+   public User() {
+	   
+   }
+
+   public User(Long id, String name, String email, String fone, LocalDate birthDate, String password) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.email = email;
+	this.fone = fone;
+	this.birthDate = birthDate;
+	this.password = password;
+   }
+
+   public Long getId() {
+	return id;
+   }
+
+   public void setId(Long id) {
+	this.id = id;
+   }
+
+   public String getName() {
+	return name;
+   }
+
+   public void setName(String name) {
+	this.name = name;
+   }
+
+   public String getEmail() {
+	return email;
+   }
+
+   public void setEmail(String email) {
+	this.email = email;
+   }
+
+   public String getFone() {
+	return fone;
+   }
+
+   public void setFone(String fone) {
+	this.fone = fone;
+   }
+
+   public LocalDate getBirthDate() {
+	return birthDate;
+   }
+
+   public void setBirthDate(LocalDate birthDate) {
+	this.birthDate = birthDate;
+   }
+
+   public String getPassword() {
+	return password;
+   }
+
+   public void setPassword(String password) {
+	this.password = password;
+   }
+   
+   
+   
+}
