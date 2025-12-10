c044fba94021860b34d1da08919deeb2eb143faf
luiz Sette <luiz.fernando.sette@gmail.com>
2025-12-10 07:40:11 -0300

    GET Products paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,4 +27,17 @@ public class ProductController { //Alguns gostam de usar Controller outros Resou
 		
 	}
 	
-}
+	//Estrutura em camadas: O controlador rest chama o product service e recebe resposta uma lista de DTO da entidade
+	//ProductService chama ProductRepository que por sua vez chama a entidade e opera o BD
+	 
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) { //O import é do spring.data.domain
+		
+		return service.findAll(pageable);
+		
+		
+	}//exemplo de uma consulta paginada
+	
+}//A customizacao do retorno da paginacao é feita do lado do front end. No postman por exemplo:
+//se eu der localhost:8080/products  e passar os argumentos ?size=12$page=2&sort=name ele vai trazer a paginacao de 12 em 12 e ja 
+//vai buscar a paginacao de numero 2 ordenada por nome
