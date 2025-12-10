428a7821ee9f0a1128c857754c47c185ca578936
luiz Sette <luiz.fernando.sette@gmail.com>
2025-12-10 08:56:30 -0300

    Post new product

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,12 +1,18 @@
 package com.devsuperior.dscommerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
@@ -19,25 +25,33 @@ public class ProductController { //Alguns gostam de usar Controller outros Resou
 	private ProductService service;
 	
 	@GetMapping(value = "/{id}")
-	public ProductDTO findById(@PathVariable Long id) {
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		
 		ProductDTO dto = service.findById(id);
-		return dto;
+		return ResponseEntity.ok(dto);   //Boas praticas customizacao do codigo de retorno da resposta
 		
 		
 	}
-	
-	//Estrutura em camadas: O controlador rest chama o product service e recebe resposta uma lista de DTO da entidade
-	//ProductService chama ProductRepository que por sua vez chama a entidade e opera o BD
-	 
+		 
 	@GetMapping
-	public Page<ProductDTO> findAll(Pageable pageable) { //O import é do spring.data.domain
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) { //O import é do spring.data.domain
 		
-		return service.findAll(pageable);
-		
+		Page<ProductDTO> dto = service.findAll(pageable);
+		return ResponseEntity.ok(dto);		
 		
 	}//exemplo de uma consulta paginada
+	
+
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 	
 }//A customizacao do retorno da paginacao é feita do lado do front end. No postman por exemplo:
 //se eu der localhost:8080/products  e passar os argumentos ?size=12$page=2&sort=name ele vai trazer a paginacao de 12 em 12 e ja 
 //vai buscar a paginacao de numero 2 ordenada por nome
+
+
+
