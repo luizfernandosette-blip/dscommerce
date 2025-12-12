4b5b58b4d79740ecb0f46ea7aab8e17c4bc994d0
luiz Sette <luiz.fernando.sette@gmail.com>
2025-12-12 07:12:25 -0300

    Customizando JSON resposta errors validacao

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -19,6 +19,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value = "/products")  //conf de um controlador rest. basta usar essas anotations > tudo q for /products vai cair aqui
 public class ProductController { //Alguns gostam de usar Controller outros Resources  ambos sao recursos REST que tratam requisicoes
@@ -45,14 +47,14 @@ public class ProductController { //Alguns gostam de usar Controller outros Resou
 	
 
 	@PostMapping
-	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+	public ResponseEntity<ProductDTO> update(@Valid @PathVariable Long id, @RequestBody ProductDTO dto) {
 		
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);   //Boas praticas customizacao do codigo de retorno da resposta
--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -2,11 +2,22 @@ package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {   //Esse DTO aqui vai trazer somente parte dos atributos da entidade product, e aqui so me interessam os 
 						//Getters
 	private Long id;
+	   						// validations faco aqui. Alem de fazer a validation aqui, no controller tenho q colocar o @Valid
+	@NotBlank(message = "Campo requerido")
+	@Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
 	private String name;
+	
+	@Size(min = 10, message = "Descricao precisa ter no minimo 10 caracteres")
 	private String description;
+	
+	@Positive(message = "O preço deve ser positivo")
 	private Double price;
 	private String imgUrl;
 	 
